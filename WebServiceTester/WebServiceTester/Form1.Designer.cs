13594fa094881dc3a6806f76c21e4d7eb416c4c5
jirikadlec2 <jirikadlec2@gmail.com>
2015-04-25 20:05:13 -0600

    WebServiceTester for testing the sending of SOAP envelope to WaterOneFlow web service

---
+++ WebServiceTester/WebServiceTester/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace WebServiceTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSiteCode = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtVariableCode = new System.Windows.Forms.TextBox();
+            this.txtStartDate = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEndDate = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtWSDL = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(21, 269);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(353, 153);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 253);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "HTTP SOAP Envelope:";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(307, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 438);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "GetValues dataset";
+            // 
+            // txtSiteCode
+            // 
+            this.txtSiteCode.Location = new System.Drawing.Point(89, 22);
+            this.txtSiteCode.Name = "txtSiteCode";
+            this.txtSiteCode.Size = new System.Drawing.Size(171, 20);
+            this.txtSiteCode.TabIndex = 1;
+            this.txtSiteCode.Text = "LittleBearRiver:USU-LBR-Mendon";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "SiteCode:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "VariableCode:";
+            // 
+            // txtVariableCode
+            // 
+            this.txtVariableCode.Location = new System.Drawing.Point(89, 59);
+            this.txtVariableCode.Name = "txtVariableCode";
+            this.txtVariableCode.Size = new System.Drawing.Size(171, 20);
+            this.txtVariableCode.TabIndex = 8;
+            this.txtVariableCode.Text = "LBR:USU4";
+            // 
+            // txtStartDate
+            // 
+            this.txtStartDate.Location = new System.Drawing.Point(149, 94);
+            this.txtStartDate.Name = "txtStartDate";
+            this.txtStartDate.Size = new System.Drawing.Size(111, 20);
+            this.txtStartDate.TabIndex = 10;
+            this.txtStartDate.Text = "2009-05-01";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "StartDate (YYYY-MM-dd)";
+            // 
+            // txtEndDate
+            // 
+            this.txtEndDate.Location = new System.Drawing.Point(149, 136);
+            this.txtEndDate.Name = "txtEndDate";
+            this.txtEndDate.Size = new System.Drawing.Size(111, 20);
+            this.txtEndDate.TabIndex = 12;
+            this.txtEndDate.Text = "2009-06-01";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "EndDate (YYYY-MM-dd)";
+            // 
+            // txtWSDL
+            // 
+            this.txtWSDL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtWSDL.Location = new System.Drawing.Point(66, 171);
+            this.txtWSDL.Name = "txtWSDL";
+            this.txtWSDL.Size = new System.Drawing.Size(308, 20);
+            this.txtWSDL.TabIndex = 14;
+            this.txtWSDL.Text = "http://icewater.usu.edu/littlebearriver/cuahsi_1_0.asmx?WSDL\r\n";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(18, 174);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "WSDL:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 455);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(353, 150);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 621);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtWSDL);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtEndDate);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtStartDate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtVariableCode);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSiteCode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "GetValues HTTP SOAP Request";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSiteCode;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtVariableCode;
+        private System.Windows.Forms.TextBox txtStartDate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEndDate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtWSDL;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WebServiceTester/WebServiceTester/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Net;
+using System.IO;
+
+namespace WebServiceTester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            /*textBox1.Text = @"<?xml version=""1.0"" encoding=""utf-8""?>" +
+@"<soap:Envelope xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:soap=""http://schemas.xmlsoap.org/soap/envelope/"">" +
+  "<soap:Body>" +
+    @"<GetValues xmlns=""http://www.cuahsi.org/his/1.0/ws/"">" +
+      "<location>LittleBearRiver:USU-LBR-Mendon</location>" +
+      "<variable>LBR:USU4</variable>" +
+      "<startDate>2009-05-01</startDate>" +
+      "<endDate>2009-06-01</endDate>" +
+      "<authToken></authToken>" +
+    "</GetValues>" +
+  "</soap:Body>" +
+"</soap:Envelope>";*/
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //get the user entered values
+            string siteCode = txtSiteCode.Text;
+            string variableCode = txtVariableCode.Text;
+            string startDate = txtStartDate.Text;
+            string endDate = txtEndDate.Text;
+            string authToken = "";
+            
+            //Construct the SOAP envelope
+            StringBuilder soapEnv = new StringBuilder();
+            soapEnv.AppendLine(@"<?xml version=""1.0"" encoding=""utf-8""?>");
+            soapEnv.AppendLine(@"<soap:Envelope xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:soap=""http://schemas.xmlsoap.org/soap/envelope/"">");
+            soapEnv.AppendLine("<soap:Body>");
+            soapEnv.AppendLine(@"<GetValues xmlns=""http://www.cuahsi.org/his/1.0/ws/"">");
+            soapEnv.AppendLine("<location>" + siteCode + "</location>");
+            soapEnv.AppendLine("<variable>" + variableCode + "</variable>");
+            soapEnv.AppendLine("<startDate>" +startDate + "</startDate>");
+            soapEnv.AppendLine("<endDate>" + endDate + "</endDate>");
+            soapEnv.AppendLine("<authToken>" + authToken + "</authToken>");
+            soapEnv.AppendLine("</GetValues>");
+            soapEnv.AppendLine("</soap:Body>");
+            soapEnv.AppendLine("</soap:Envelope>");
+
+            //display the soap envelope in the textBox
+            textBox1.Text = soapEnv.ToString();
+
+            //call the Http Soap request
+            HttpSOAPRequest(textBox1.Text, null);
+        }
+
+
+        void HttpSOAPRequest(String xmlfile, string proxy)
+        {
+            //string url = "http://water.sdsc.edu/hiscentral/webservices/hiscentral.asmx";
+
+            //string url = "http://icewater.usu.edu/littlebearriver/cuahsi_1_0.asmx";
+
+            string url = txtWSDL.Text.Trim().ToLower();
+            //for http request, we need to remove the ?WSDL part from the url
+            if (url.EndsWith("?wsdl"))
+            {
+                url = url.Replace("?wsdl", "");
+            }
+
+            //send the SOAP envelope to the service as a xml document
+            XmlDocument doc = new XmlDocument();
+            doc.Load(new System.IO.StringReader(textBox1.Text));
+            
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            if (proxy != null) req.Proxy = new WebProxy(proxy, true);
+            // if SOAPAction header is required, add it here...
+
+            //this is the valid SoapAction header for GetValues web method
+            req.Headers.Add("SOAPAction", @"http://www.cuahsi.org/his/1.0/ws/GetValues");
+
+            req.ContentType = "text/xml;charset=\"utf-8\"";
+            req.Accept = "text/xml";
+            req.Method = "POST";
+            Stream stm = req.GetRequestStream();
+            doc.Save(stm);
+            stm.Close();
+            WebResponse resp = req.GetResponse();
+            stm = resp.GetResponseStream();
+            StreamReader r = new StreamReader(stm);
+            
+            // process SOAP return doc here
+            string result = r.ReadToEnd();
+
+            //extract the soap body result
+            XmlDocument resultDoc = new XmlDocument();
+            resultDoc.LoadXml(result);
+            string soapBody = resultDoc.DocumentElement.ChildNodes[1].InnerText;
+
+            //extract the dataSet
+            DataSet ds = new DataSet();
+            XmlReader reader = XmlReader.Create(new StringReader(soapBody));
+            ds.ReadXml(reader);
+
+            dataGridView1.DataSource = ds.Tables["Value"];
+        }
+
+    }
+}
